4e13fe0dd983093a1229269287caaa1be730c33b
iliyashev <iliya.shevolaev@gmail.com>
2024-10-15 11:26:04 +0300

    components backup

--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,20 +1,9 @@
 <template>
     <div>
-        <CreateComponent></CreateComponent>
-
-        <IndexComponent ref="index"></IndexComponent>
+        
     </div>
 </template>
 
 <script>
-import CreateComponent from "./CreateComponent.vue";
-import IndexComponent from "./IndexComponent.vue";
 
-export default {
-    components: {
-        CreateComponent,
-        IndexComponent,
-    },
-    
-};
 </script>
